d906527810157580e4aaa1c5196d8da6ce7e37ed
Tymon <Tymon1996@gmail.com>
2015-06-13 21:39:54 +1000

    .

--- src/ResourcesFolder/Apps/Thing.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('../../Classes/Layout.php');
-
-$content = '<iframe width="420" height="315" src="https://www.youtube.com/embed/J9FImc2LOr8?autoplay=1&loop=1" frameborder="0" allowfullscreen></iframe>';
-
-$AboutUs = new Layout(Null, $content, Null, Null, Null, Null, 2);
-echo $AboutUs->Layout();
