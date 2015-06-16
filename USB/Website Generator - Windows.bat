50b9e336bcbcc69a4b199f8f4de2858cc30e9eeb
Tymon <Tymon1996@gmail.com>
2015-06-17 00:44:28 +1000

    Reformat final

---
+++ USB/Website Generator - Windows.bat
@@ -0,0 +1,5 @@
+REM Change the directory to src
+chdir src
+REM Run The HTML Converter
+REM "%cd:~0,2%\php\php-win.exe" -f %cd:~0,2%\src\HTMLConverter.php
+"C:\Users\Tymon\IdeaProjects\RoverWebsite4\ER Website\php\php-win.exe" -f C:\Users\Tymon\IdeaProjects\RoverWebsite4\ER Website\src\HTMLConverter.php
